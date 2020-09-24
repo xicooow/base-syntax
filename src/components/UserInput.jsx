1166c284ea66fa10429b41a22e03b6a16bdc36b6
fosilva <fosilva@daitan.com>
2020-09-24 15:34:22 -0300

    Updates

--- src/components/UserInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserInput = (props) => {
-  return (
-    <input className='custom-inputs' placeholder='Change me...' onChange={props.changeUsername} value={props.username} />
-  );
-};
-
-export default UserInput;
